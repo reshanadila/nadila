7604a3168d75eac625533688a859bace0c9d1e8a
DidinMahfuddin <151610194@smkassalaambandung.sch.id>
2017-07-27 23:55:17 -0700

    siswa model

--- database/seeds/SiswaTableSeeder.php
+++ database/seeds/SiswaTableSeeder.php
@@ -14,7 +14,9 @@ class SiswaTableSeeder extends Seeder
          $siswa = [
         ['nama'=>'resha nadila', 'alamat'=>'kp sekeandur','tgl'=>'2000-07-31'],
         ['nama'=>'windi novianti', 'alamat'=>'kp cilebak','tgl'=>'2000-07-29'],
-        ['nama'=>'sulis', 'alamat'=>'kp sukamenak','tgl'=>'2000-11-05']
+        ['nama'=>'sulis', 'alamat'=>'kp sukamenak','tgl'=>'2000-11-05'],
+        ['nama'=>'lindiyani', 'alamat'=>'kp cilebak','tgl'=>'2000-11-11'],
+        ['nama'=>'nadiyah', 'alamat'=>'kp sayuran','tgl'=>'2000-12-05']
             
                   ];
                   DB::table('siswa')->insert($siswa);
